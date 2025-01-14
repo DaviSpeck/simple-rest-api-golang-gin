28fa8cf9877f210f5f3219e8455b75ae3fb5128d
davispeck <davi.speck86@gmail.com>
2025-01-14 07:38:26 -0300

    setting GET route with mock and config database

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,28 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"simple-rest-api-golang-gin/controller"
+	"simple-rest-api-golang-gin/usecase"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 
 	server := gin.Default()
+
+	ProductUseCase := usecase.NewProductUseCase()
+
+	// Controllers
+	ProductController := controller.NewProductController(ProductUseCase)
 
 	server.GET("/ping", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
+
+	server.GET("/products", ProductController.GetProducts)
 
 	server.Run(":8000")
 
